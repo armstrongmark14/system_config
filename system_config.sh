f38d2278274b4c04f75805966085d6e562ec6752
Mark Armstrong <armstrongmark14@gmail.com>
2019-01-30 10:54:03 -0500

    Added terminal profile and terminal padding css file

--- system_config.sh
+++ system_config.sh
@@ -10,9 +10,15 @@ mkdir ~/.icons ~/.ssh ~/.themes
 cd ~/code
 git clone https://github.com/armstrongmark14/system_config.git
 
+cd system_config
+
 # Moving config files
-mv ~/code/system_config/.bashrc ~/.bashrc
-mv ~/code/system_config/.vimrc ~/.vimrc
+cp .bashrc ~/.bashrc
+cp .vimrc ~/.vimrc
+cp gtk.css ~/.config/gtk-3.0/gtk.css
+
+# Loading the terminal preferences, still need to install fonts manually
+dconf load /org/gnome/terminal/legacy/profiles:/ < terminal_profile.dconf
 
 # Installing vim plugins
 sh ~/code/system_config/plugins/plugins.sh
